0f146b5063274b9b5cf252dd012f9b089be26b62
unknown <shokirovbekzod6090@gmail.com>
2024-12-07 00:17:13 +0900

    BRR: Initial Setup

---
+++ train.js
@@ -0,0 +1,38 @@
+// Compiled Language: Java, GoLang, C, C++, C#, Rust => kompyutr tuliq mashina tiliga ugiriladi & Running
+// (xatolik tezda aniq buladi va Run qilib utirilmiydi)
+
+
+// Interpreted Language: NodJS, Python, PHP, Ruby => jarayon soda tugridan tugri Running
+// ( minus taraflari mavjud xatolarni kurishda)
+
+//typescript= javascript+typescript 
+
+//interface 
+// interface Person {
+//     name: string,
+//     age: Number,
+//     nation: string
+// }
+
+// const person: Person ={
+//     name: 'Martin',
+//     age: 30,
+//     nation: 'Australian'
+// }
+
+// let skills: (number | string) [];
+// skills = ['Problem Sovling', 'Software Design','Programming', 100];
+
+
+// class Person {
+//     age: Number;
+//     firstName: string;
+//     lastName: string;
+
+//     constructor(age: number, firstName: string, lastName: string) {
+//         this.age = age;
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+// }
+// const person1 = new Person(30, 'Martin', 'Robertson');
