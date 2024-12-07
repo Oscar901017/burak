89838cc9432beec74b4ebc12f7a58bb87bd93dd5
unknown <shokirovbekzod6090@gmail.com>
2024-12-07 20:26:20 +0900

    fix: typescript va patternlar haqida

--- train.js
+++ train.js
@@ -1,3 +1,28 @@
+// TASK G:
+
+// Yagona parametrga ega function tuzing.
+// Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+// Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+// MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+// Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+// Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+
+
+function getHighestIndex(arr) {
+    
+    let highestValue = Math.max(...arr);
+    
+    return arr.indexOf(highestValue);
+}
+
+console.log(getHighestIndex([5, 21, 12, 21, 8])); 
+
+
+
+
+
+
 // Compiled Language: Java, GoLang, C, C++, C#, Rust => kompyutr tuliq mashina tiliga ugiriladi & Running
 // (xatolik tezda aniq buladi va Run qilib utirilmiydi)
 
@@ -203,22 +228,4 @@
 
 // console.log(findDoublers("abc")); 
 
-TASK G:
 
-Yagona parametrga ega function tuzing.
-Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
-Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
-
-MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
-Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
-Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
-
-
-function getHighestIndex(arr) {
-    
-    let highestValue = Math.max(...arr);
-    
-    return arr.indexOf(highestValue);
-}
-
-console.log(getHighestIndex([5, 21, 12, 21, 8])); 
