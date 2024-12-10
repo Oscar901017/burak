83cb525cd3f90a49ec268d1cae7e6de0dd0d4b08
unknown <shokirovbekzod6090@gmail.com>
2024-12-10 09:15:02 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,2 @@
-// console.log("EXUCUTED");
-
-// import moment from 'moment';
-
-// const currentTime = moment().format('YYYY MM DD');
-// console.log(currentTime);
-
-// const person: string ="Martin";
-// const count: number= 100;
-
-
-//Architectural: MVC, DI(Dependecy injection),MVP
-
-// MVS = Model View controller
-
-// Disgn pattern: MiddLeware, Decator
-
+import dotevn from "dotenv";
+dotevn.config();
