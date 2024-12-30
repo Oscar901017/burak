a8f354f6efa007b62c7d4a42e43969c6dffff299
unknown <shokirovbekzod6090@gmail.com>
2024-12-31 02:32:43 +0900

    feat: signup

--- train.js
+++ train.js
@@ -1,16 +1,42 @@
+// TASK-N: 
+
+// Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
+// MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
+
+function palindromCheck(str) {
+   
+   let cleanedStr = str.toLowerCase().replace(/\s+/g, '');
+   
+   
+   let reversedStr = cleanedStr.split('').reverse().join('');
+   
+   
+   return cleanedStr === reversedStr;
+}
+
+
+console.log(palindromCheck("dad"));  
+console.log(palindromCheck("son"));  
+
+
+
+
+
+
+
 // TASK M: 
 
 // Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
 // MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
 
-function getSquareNumbers(arr) {
-   return arr.map(num => ({
-     number: num,
-     square: num * num
-   }));
- }
+// function getSquareNumbers(arr) {
+//    return arr.map(num => ({
+//      number: num,
+//      square: num * num
+//    }));
+//  }
 
- console.log(getSquareNumbers([1, 2, 3]));
+//  console.log(getSquareNumbers([1, 2, 3]));
 
  
 
