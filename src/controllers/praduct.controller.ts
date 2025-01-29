e6e844005edfc4fc021ab4e64c17b5f17b4e4bea
unknown <shokirovbekzod6090@gmail.com>
2025-01-29 14:01:43 +0900

    feat: develop get Products rest Api

--- src/controllers/praduct.controller.ts
+++ src/controllers/praduct.controller.ts
@@ -3,77 +3,101 @@ import Errors, { Httpcode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
 import { AdminRequest } from "../libs/types/member";
-import { ProductInput } from "../libs/types/product";
+import { ProductInput, ProductInquiry } from "../libs/types/product";
+import { ProductCollection } from "../libs/enums/product.enum";
 
-const productService = new  ProductService();
-const productController: T = {}; 
+const productService = new ProductService();
+const productController: T = {};
 
+/** SPA */
+productController.getProducts = async (req: Request, res: Response) => {
+  try {
+    console.log("getProducts");
+    const { page, limit, order, productCollection, search } = req.query;
+    console.log(`page: ${page}, order: ${order}`);
+    const inquiry: ProductInquiry = {
+      order: String(order),
+      page: Number(page),
+      limit: Number(limit),
+    };
+    if (productCollection)
+      inquiry.productCollection = productCollection as ProductCollection;
+    if (search) inquiry.search = String(search);
 
-    /** SPA */
+    const result = await productService.getProducts(inquiry);
 
-    //** SSR */
-
-productController.getAllProducts = async (req: Request, res: Response) => {
-   
-    try {
-        console.log("getAllProducts");
-        const data  = await productService.getAllProducts();
-        console.log("data:", data);
-
-
-        res.render("products", {products: data});
-    } catch (err) {
-        console.log("Error, getAllProducts", err);
-        if (err instanceof Errors) res.status(err.code).json(err);
-        else res.status(Errors.standart.code).json(Errors.standart);
-    }
+    // const data  = await productService.getAllProducts();
+    const query = req.query;
+    console.log("req.query:", query);
+    res.status(Httpcode.OK).json(result);
+  } catch (err) {
+    console.log("Error, getProducts", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart);
+  }
 };
 
-productController.createNewProduct = async (req: AdminRequest , res: Response) => {
-   
-    try {
-        console.log("createNewProduct");
-        console.log("req.files:", req.files);
+//** SSR */
 
+productController.getAllProducts = async (req: Request, res: Response) => {
+  try {
+    console.log("getAllProducts");
+    const data = await productService.getAllProducts();
+    console.log("data:", data);
 
-    if (!req.files?.length) throw new Errors(Httpcode.INTERNAL_SERVES_ERROR, Message.CREATE_FAILED);  
-     
-    const data:ProductInput = req.body;
-            data.productImages = req.files?.map((ele) => {
-                return ele.path.replace(/\\/g, "/");            
-            });
+    res.render("products", { products: data });
+  } catch (err) {
+    console.log("Error, getAllProducts", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart);
+  }
+};
 
+productController.createNewProduct = async (
+  req: AdminRequest,
+  res: Response
+) => {
+  try {
+    console.log("createNewProduct");
+    console.log("req.files:", req.files);
 
-            await ProductService.createNewProduct(data);
-            res.send(`<script> alert("Sucessful creation!); window.location.replace('/admin/product/all') </script>`);
-            console.log("data:", data);
-        res.send("DONE");
-        
-    } catch (err) {
-        console.log("Error, createNewProduct", err);
-        const message = err instanceof Errors ? err.message:Message.SOMTHING_WENT_WRONG;   
-        res.send(`<script> alert("${message}"); window.location.replace('/admin/product/all') </script>`);
-            
-    }
+    if (!req.files?.length)
+      throw new Errors(Httpcode.INTERNAL_SERVES_ERROR, Message.CREATE_FAILED);
+
+    const data: ProductInput = req.body;
+    data.productImages = req.files?.map((ele) => {
+      return ele.path.replace(/\\/g, "/");
+    });
+
+    await ProductService.createNewProduct(data);
+    res.send(
+      `<script> alert("Sucessful creation!); window.location.replace('/admin/product/all') </script>`
+    );
+    console.log("data:", data);
+    res.send("DONE");
+  } catch (err) {
+    console.log("Error, createNewProduct", err);
+    const message =
+      err instanceof Errors ? err.message : Message.SOMTHING_WENT_WRONG;
+    res.send(
+      `<script> alert("${message}"); window.location.replace('/admin/product/all') </script>`
+    );
+  }
 };
 
 productController.updateChosenProduct = async (req: Request, res: Response) => {
-    
-    try {
-        console.log("updateChosenProduct");
-        const id = req.params.id;
-        
-        const result = await productService.updateChosenProduct(id,req.body)
+  try {
+    console.log("updateChosenProduct");
+    const id = req.params.id;
 
-        res.status(Httpcode.OK).json({data: result});
-        
-    } catch (err) {
-        console.log("Error, updateChosenProduct", err);
-        if (err instanceof Errors) res.status(err.code).json(err);
-        else res.status(Errors.standart.code).json(Errors.standart);
-    }
+    const result = await productService.updateChosenProduct(id, req.body);
+
+    res.status(Httpcode.OK).json({ data: result });
+  } catch (err) {
+    console.log("Error, updateChosenProduct", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart);
+  }
 };
 
 export default productController;
-
-
--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,57 +1,81 @@
 import Errors from "../libs/Errors";
-import { Product, ProductInput } from "../libs/types/product";
+import { Product, ProductInput, ProductInquiry } from "../libs/types/product";
 import ProductModels from "../schema/Product.models";
 import { Httpcode } from "../libs/Errors";
 import { Message } from "../libs/Errors";
 import { shapeIntoMongooseObjectId } from "../libs/config";
+import { T } from "../libs/types/common";
+import { ProductStatus } from "../libs/enums/product.enum";
 
 class PraductService {
-    static updateChosenProduct(body: any) {
-        throw new Error("Method not implemented.");
-    }
-    static createNewProduct(data: ProductInput) {
-        throw new Error("Method not implemented.");
-    }
-    private readonly productModel;
+  static updateChosenProduct(body: any) {
+    throw new Error("Method not implemented.");
+  }
+  static createNewProduct(data: ProductInput) {
+    throw new Error("Method not implemented.");
+  }
+  private readonly productModel;
+
+  constructor() {
+    this.productModel = ProductModels;
+  }
+
+  /** SPA */
+  public async getProducts(inquiry: ProductInquiry): Promise<Product[]> {
     
+    const match: T = { productStatus: ProductStatus.PROCESS };
+    if (inquiry.productCollection)
+      match.productCollection = inquiry.productCollection;
 
-    constructor() {
-        this.productModel = ProductModels;
+    if(inquiry.search) {
+        match.productName = {$regex: new RegExp(inquiry.search, "i")}; 
     }
+    const sort: T =
+      inquiry.order === "productPrice"
+        ? { [inquiry.order]: 1 }
+        : { [inquiry.order]: -1 };
 
-    /** SPA */
+    const result = await this.productModel.aggregate([
+        {$match:match},
+        {$sort: sort},
+        {$skip: (inquiry.page * 1 - 1) * inquiry.limit},
+        {$limit:inquiry.limit * 1},
 
-    //** SSR */
+    ]).exec();
 
-    public async getAllProducts(): Promise<Product[]> {
-        const result = await this.productModel
-        .find()
-        .exec();
-        if(!result) throw new Errors(Httpcode.NOT_FOUND,Message.NO_DATA_FOUND);
-        return result;
+    if(!result) throw  new Errors(Httpcode.NOT_FOUND, Message.NO_DATA_FOUND); 
+    return result;
+  }
+
+  //** SSR */
+
+  public async getAllProducts(): Promise<Product[]> {
+    const result = await this.productModel.find().exec();
+    if (!result) throw new Errors(Httpcode.NOT_FOUND, Message.NO_DATA_FOUND);
+    return result;
+  }
+
+  public async createNewProduct(input: ProductInput): Promise<Product> {
+    try {
+      return await this.productModel.create(input);
+    } catch (err) {
+      console.error("Error, model:createNewProduct:", err);
+      throw new Errors(Httpcode.BAD_REQUIST, Message.CREATE_FAILED);
     }
+  }
 
-    public async createNewProduct(input: ProductInput):Promise<Product> {
-        try {
-            return await this.productModel.create(input);
-        }catch(err) {
-            console.error("Error, model:createNewProduct:", err);
-            throw new Errors(Httpcode.BAD_REQUIST, Message.CREATE_FAILED);
-        }
-    }
-
-
-public async updateChosenProduct(
-    id:string, 
+  public async updateChosenProduct(
+    id: string,
     input: ProductInput
-):Promise<Product> {
+  ): Promise<Product> {
     // string => objectid uzgartirish k.k
     id = shapeIntoMongooseObjectId(id);
-    const result = await this.productModel.findByIdAndUpdate({_id: id}, input, {new: true})
-    .exec();
-    if(!result) throw new Errors(Httpcode.NOT_MODIFIED, Message.UPDATE_FAILED);   
+    const result = await this.productModel
+      .findByIdAndUpdate({ _id: id }, input, { new: true })
+      .exec();
+    if (!result) throw new Errors(Httpcode.NOT_MODIFIED, Message.UPDATE_FAILED);
     return result;
-}
+  }
 }
 
 export default PraductService;
